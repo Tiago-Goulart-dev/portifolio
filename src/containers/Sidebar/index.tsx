4dc39e58c826b754b3487689bec6a7d4a5a6d2e6
Tiago Goulart <tiagogoulart3012@gmail.com>
2024-03-13 10:44:47 -0300

    Estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/tiago-goulart-dev.png" />
-    <Titulo fontSize={20}>Tiago Goulart</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Tiago Goulart</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        tiago-goulart-dev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
